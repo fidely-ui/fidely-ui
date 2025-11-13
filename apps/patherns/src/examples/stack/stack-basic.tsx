77a84315697186d75d4715242be77c373f9cdc51
justice chimobi <chimobi.justice@gmail.com>
2025-11-13 10:13:16 +0100

    docs: added stacks, grid documentation

---
+++ apps/patherns/src/examples/stack/stack-basic.tsx
@@ -0,0 +1,12 @@
+import { Box, Stack } from '@snaps-ui/react'
+
+export const StackBasic = () => {
+  return (
+    <Stack>
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+    </Stack>
+  )
+}
---
+++ apps/patherns/src/examples/stack/stack-hstack.tsx
@@ -0,0 +1,12 @@
+import { Box, HStack } from '@snaps-ui/react'
+
+export const StackHstack = () => {
+  return (
+    <HStack>
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+      <Box height={'50px'} bg={'bg.emphasized'} />
+    </HStack>
+  )
+}
