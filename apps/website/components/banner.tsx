f8faaf5dd99ae60ac3bd73a58812622a3bcbfd58
chimobi-justice <chimobi.justice@gmail.com>
2026-02-04 10:41:15 +0100

    Modify Banner component to link to Fidely UI V2
    Updated the Banner component to reflect the older version of Fidely UI and added a link to the latest version.

--- apps/website/components/banner.tsx
+++ apps/website/components/banner.tsx
@@ -1,17 +1,23 @@
-'use client'
-
+import Link from 'next/link'
 import { Box } from '@fidely-ui/react/box'
 import { Center } from '@fidely-ui/react/center'
 import { Text } from '@fidely-ui/react/text'
-import { Badge } from '@fidely-ui/react/badge'
+// import { Badge } from '@fidely-ui/react/badge'
+import { Button } from '@fidely-ui/react'
 
 export const Banner = () => {
   return (
-    <Box bg="orange.9" p="2" w="full">
+    <Box bg='orange.9' p='2' w='full'>
       <Center>
-        <Text fontWeight="bold" color="white">
-          Unlock faster development with Fidely UI Pro components{' '}
-          <Badge border="none">Coming soon</Badge>
+        <Text fontWeight='bold' color='white'>
+          You are viewing the older version of Fidely UI for latest visit
+          <Button asChild size='xs' ml='4'>
+            <Link 
+              href='https://fidely-ui.vercel.app/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Fidely UI V2</Link>
+          </Button>
         </Text>
       </Center>
     </Box>
