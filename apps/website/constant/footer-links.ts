a3e1b51d315213025710a8adc60f0ea8ce0cdd7b
justice chimobi <chimobi.justice@gmail.com>
2026-03-02 23:01:47 +0100

    feat: update footer section links

---
+++ apps/website/constant/footer-links.ts
@@ -0,0 +1,38 @@
+export const DOCUMENTATION_LINK = [
+  { href: '/docs/getting-started/installation', label: 'Getting Started' },
+  { href: '/docs/components/accordion', label: 'Components' },
+  { href: '/docs/theming/customization', label: 'Theming' },
+  // { href: '/blogs', label: 'Blog' },
+]
+
+export const COMMUNITY_LINK = [
+  // { href: '#', label: 'Discord' },
+  { href: 'https://x.com/FidelyUi', label: 'Twitter' },
+  { href: 'https://www.github.com/chimobi-justice', label: 'Follow on Github' },
+  {
+    href: 'https://github.com/fidely-ui/fidely-ui/discussions',
+    label: 'Github Discussions',
+  },
+]
+
+export const RESOURCES_LINK = [
+  { href: '#', label: 'UI Blocks' },
+  { href: '#', label: 'Templates' },
+  { href: '#', label: 'Commerce' },
+]
+
+export const ABOUT_LINK = [
+  { href: 'https://ko-fi.com/fidely_ui', label: 'Kofi' },
+  {
+    href: 'https://github.com/fidely-ui/fidely-ui/blob/main/CHANGELOG.md',
+    label: 'Changelog',
+  },
+  {
+    href: 'https://github.com/fidely-ui/fidely-ui/blob/main/CONTRIBUTING.md',
+    label: 'Contribute',
+  },
+  {
+    href: 'https://github.com/fidely-ui/fidely-ui/releases',
+    label: 'Github Release',
+  },
+]
