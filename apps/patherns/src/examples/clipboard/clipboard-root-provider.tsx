17fe83d43c0c8513ff69e2e34fc880b8836479b0
justice chimobi <chimobi.justice@gmail.com>
2026-04-26 11:40:13 +0100

    docs(clipboard): update clipboard root provider

--- apps/patherns/src/examples/clipboard/clipboard-root-provider.tsx
+++ apps/patherns/src/examples/clipboard/clipboard-root-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Clipboard, useClipboard } from '@fidely-ui/react'
+import { Clipboard, IconButton, Input, useClipboard } from '@fidely-ui/react'
+import { FaRegCopy } from 'react-icons/fa6'
 
 export const ClipboardRootProvider = () => {
   const clipboard = useClipboard({ value: 'https://fidely-ui.vercel.app/' })
@@ -9,9 +10,15 @@ export const ClipboardRootProvider = () => {
     <Clipboard.RootProvider value={clipboard}>
       <Clipboard.Label>Copy this link</Clipboard.Label>
       <Clipboard.Control>
-        <Clipboard.Input />
-        <Clipboard.Trigger>
-          <Clipboard.Indicator />
+        <Clipboard.Input asChild>
+          <Input />
+        </Clipboard.Input>
+        <Clipboard.Trigger asChild>
+          <IconButton variant="subtle">
+            <Clipboard.Indicator>
+              <FaRegCopy />
+            </Clipboard.Indicator>
+          </IconButton>
         </Clipboard.Trigger>
       </Clipboard.Control>
     </Clipboard.RootProvider>
